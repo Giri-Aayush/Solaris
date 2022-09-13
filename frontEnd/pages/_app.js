5c258b4005c83d73b9e77d4f37144e1742cfa442
Himanshu <goyalhimanshu424@gmail.com>
2022-09-14 00:54:05 +0530

    first

---
+++ frontEnd/pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+
+import '../public/assets/vendor/perfect-scrollbar.css'
+import '../public/assets/vendor/fix-footer.css'
+
+import '../public/assets/css/material-icons.css'
+import '../public/assets/css/fontawesome.css'
+import '../public/assets/css/preloader.css'
+import '../public/assets/css/app.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
